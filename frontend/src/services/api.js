c8bb06119a49327a1fbd628dedff5a70308ccdbe
kunalkhare21 <kunalkhare123456@gmail.com>
2026-05-18 23:10:17 +0530

    fix client side routing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const API = axios.create({
     // baseURL: "http://localhost:5000/api"
-    baseURL: "https://music-instrument-tutoring-network.onrender.com"
+    baseURL: "https://music-instrument-tutoring-network.onrender.com/api"
 })
 
 export default API
